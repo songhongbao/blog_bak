4ad369cca775ea209420597e71767bdf568247e1
ran <songhongyubing@163.com>
2017-11-01 12:30:01 +0800

    add

--- wp-includes/post-thumbnail-template.php
+++ wp-includes/post-thumbnail-template.php
@@ -59,7 +59,11 @@ function get_post_thumbnail_id( $post = null ) {
  * @param string|array $attr Optional. Query string or array of attributes. Default empty.
  */
 function the_post_thumbnail( $size = 'post-thumbnail', $attr = '' ) {
-	echo get_the_post_thumbnail( null, $size, $attr );
+        $url = get_the_post_thumbnail( null, $size, $attr );
+        if(!$_SERVER['HTTPS']) {
+                $url = str_replace('love.ranshy.com/wp-content/uploads', '7xi8tc.com1.z0.glb.clouddn.com', $url);
+        }
+        echo $url;
 }
 
 /**
