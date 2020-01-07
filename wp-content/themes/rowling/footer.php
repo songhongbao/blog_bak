dcf9801ad9c66ae61988037b170241f98331de69
ran <songhongyubing@163.com>
2020-01-07 11:30:01 +0800

    add

--- wp-content/themes/rowling/footer.php
+++ wp-content/themes/rowling/footer.php
@@ -2,7 +2,7 @@
 			
 	<div class="section-inner">
 		
-		<p class="attribution"><a href="http://www.miitbeian.gov.cn/">黑ICP备15001596号</a></p>
+		<p class="attribution"><a href="http://www.beian.miit.gov.cn">黑ICP备15001596号</a></p>
 		
 	</div>
 	
